596d1df13576830a0d04f31cb52816dec9e12ef2
Michał Francuz <104276165+michal9696@users.noreply.github.com>
2023-01-09 23:56:57 +0100

    Learning of JavaFX - Build tasks app - add new view to create new task, animation for add button

--- src/main/java/controller/AddItemController.java
+++ src/main/java/controller/AddItemController.java
@@ -1,7 +1,13 @@
 package controller;
 
+import animations.Shaker;
+import javafx.animation.FadeTransition;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,8 +22,35 @@ public class AddItemController {
     private ImageView addButton;
 
     @FXML
+    private Label notTaskLabel;
+
+    @FXML
     void initialize() {
 
+        addButton.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            Shaker buttonShaker = new Shaker(addButton);
+            buttonShaker.shake();
+
+            FadeTransition buttonTransition = new FadeTransition(Duration.millis(2500), addButton);
+            FadeTransition labelTransition = new FadeTransition(Duration.millis(2500), notTaskLabel);
+
+            System.out.println("Added button Clicked");
+            addButton.relocate(0, 0);
+            notTaskLabel.relocate(0, 55);
+
+            addButton.setOpacity(0);
+            notTaskLabel.setOpacity(0);
+
+            buttonTransition.setFromValue(1f);
+            buttonTransition.setToValue(0f);
+            buttonTransition.setCycleCount(2);
+            buttonTransition.play();
+
+            labelTransition.setFromValue(1f);
+            labelTransition.setToValue(0f);
+            labelTransition.setCycleCount(2);
+            labelTransition.play();
+        });
 
     }
 }
