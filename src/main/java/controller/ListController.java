ad2ab76cd09a90739ee9d87ea4db670a8ebc9d29
Michał Francuz <104276165+michal9696@users.noreply.github.com>
2023-01-18 23:52:44 +0100

    Learning of JavaFX - Build tasks app - enhanced my list of task using JavaFX libraries

--- src/main/java/controller/ListController.java
+++ src/main/java/controller/ListController.java
@@ -1,12 +1,20 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListCell;
 import com.jfoenix.controls.JFXListView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -53,5 +61,38 @@ public class ListController {
 
         listTask.setItems(listview);
 
+        listTask.setCellFactory(param -> new JFXCell());
+
     }
+
+    static class JFXCell extends JFXListCell<String> {
+        //Hbox = Horizontal Box
+        HBox hBox = new HBox();
+        Button helloButton = new Button("Ok");
+        Label task = new Label();
+
+        Pane pane = new Pane();
+
+        Image icon = new Image("/com/example/dutymanagement/assets/icons8-add-new-64.png");
+        ImageView iconImg = new ImageView(icon);
+
+        public JFXCell() {
+            super();
+
+            hBox.getChildren().addAll(iconImg, task, helloButton);
+            HBox.setHgrow(pane, Priority.ALWAYS);
+        }
+
+        public void updateItem(String taskName, boolean empty) {
+            super.updateItem(taskName, empty);
+            setText(null);
+            setGraphic(null);
+
+            if (taskName != null && !empty) {
+                task.setText(taskName);
+                setGraphic(hBox);
+            }
+        }
+    }
+
 }
